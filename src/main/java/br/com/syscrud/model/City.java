f833596f72240ddc2d805a20b683e84b55e39496
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-04-15 14:33:31 -0300

    Finish City class

--- src/main/java/br/com/syscrud/model/City.java
+++ src/main/java/br/com/syscrud/model/City.java
@@ -1,5 +1,75 @@
 package br.com.syscrud.model;
 
-public class City {
+import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "city")
+public class City implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "name")
+	@Length(min = 2, max = 25)
+	@NotBlank
+	private String name;
+	
+	@Column(name = "population", nullable = false)
+	@Length(min = 1, max = 25000000)
+	@NotNull(message = "Population")
+	private Integer population;
+	
+	@ManyToOne
+	@JoinColumn(name = "country_id")
+	private Country country;
+
+	public City() {}
+
+	public City(String name, Integer population, Country country) {
+		this.name = name;
+		this.population = population;
+		this.country = country;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+	
 }
