31048de4595be52641bbca26235ddcd7a675a494
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-04-15 14:56:06 -0300

    Finish StateRepository

--- src/main/java/br/com/syscrud/repository/StateRepository.java
+++ src/main/java/br/com/syscrud/repository/StateRepository.java
@@ -1,5 +1,11 @@
 package br.com.syscrud.repository;
 
-public class StateRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.syscrud.model.State;
+
+@Repository
+public interface StateRepository extends JpaRepository<State, Long>{
 
 }
