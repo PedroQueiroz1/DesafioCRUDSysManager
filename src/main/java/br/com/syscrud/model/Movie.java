1cff07d8bd6b8a027153adb636c7671a781bf759
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-04-15 18:27:01 -0300

    Finish Movie class

--- src/main/java/br/com/syscrud/model/Movie.java
+++ src/main/java/br/com/syscrud/model/Movie.java
@@ -18,4 +18,27 @@ public class Movie extends Product implements Serializable{
 	@Column(name = "duration")
 	private Integer duration;
 	
+	public Movie() {}
+
+	public Movie(String director, Integer duration) {
+		this.director = director;
+		this.duration = duration;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public Integer getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
+	
 }
