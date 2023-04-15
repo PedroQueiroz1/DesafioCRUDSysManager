281880ad2198b31bb677ded0d4e5ce2457ad6fda
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-04-15 14:55:05 -0300

    Finish CountryRepository

--- src/main/java/br/com/syscrud/repository/CountryRepository.java
+++ src/main/java/br/com/syscrud/repository/CountryRepository.java
@@ -1,5 +1,11 @@
 package br.com.syscrud.repository;
 
-public class CountryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.syscrud.model.Country;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long>{
 
 }
