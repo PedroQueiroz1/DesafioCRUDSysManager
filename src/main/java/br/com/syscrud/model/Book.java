1b1712fa22f79b1890666a7bd70a8549c135bdd8
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-04-15 18:27:43 -0300

    Add extends and implements to Book class

--- src/main/java/br/com/syscrud/model/Book.java
+++ src/main/java/br/com/syscrud/model/Book.java
@@ -1,13 +1,17 @@
 package br.com.syscrud.model;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "book")
-public class Book {
+public class Book extends Product implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+
 	@Column(name = "author")
 	private String author;
 	
