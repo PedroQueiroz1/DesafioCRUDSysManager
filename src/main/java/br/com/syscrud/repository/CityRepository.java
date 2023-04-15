0f312fa46638e07083e66f8aa4a43681742ba5ae
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-04-15 14:54:13 -0300

    Finish CityRepository

--- src/main/java/br/com/syscrud/repository/CityRepository.java
+++ src/main/java/br/com/syscrud/repository/CityRepository.java
@@ -1,5 +1,11 @@
 package br.com.syscrud.repository;
 
-public class CityRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.syscrud.model.City;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Long>{
 
 }
