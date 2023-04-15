9101aa27bf9325015aa742d5105cf16ac05d5829
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-04-15 14:15:33 -0300

    Finish Country class

--- src/main/java/br/com/syscrud/model/Country.java
+++ src/main/java/br/com/syscrud/model/Country.java
@@ -1,7 +1,82 @@
 package br.com.syscrud.model;
 
-public class Country {
-	
-	
+import java.io.Serializable;
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "country")
+public class Country implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name = "name", nullable = false, length = 25)
+	private String name;
+	
+	@Column(name = "continent", nullable = false, length = 15)
+	private String continent;
+	
+	@OneToMany(mappedBy = "country")
+	private List<City> cities;
+	
+	
+	public Country() {}
+	
+	public Country(String name, String continent, List<City> cities) {
+		this.name = name;
+		this.continent = continent;
+		this.cities = cities;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getContinent() {
+		return continent;
+	}
+
+
+	public void setContinent(String continent) {
+		this.continent = continent;
+	}
+
+
+	public List<City> getCities() {
+		return cities;
+	}
+
+
+	public void setCities(List<City> cities) {
+		this.cities = cities;
+	}
+	
 }
