db127ef9bb41eb666fe5afd4503739639b626a43
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-04-15 18:37:35 -0300

    Add findAll to ProductController

--- src/main/java/br/com/syscrud/controller/ProductController.java
+++ src/main/java/br/com/syscrud/controller/ProductController.java
@@ -1,5 +1,23 @@
 package br.com.syscrud.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.syscrud.model.Product;
+import br.com.syscrud.service.ProductService;
+
+@RestController
 public class ProductController {
 
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping
+	public List<Product> findAll() {
+		return service.findAll();
+	}
+	
 }
