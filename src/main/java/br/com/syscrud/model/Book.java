ab6c3b53ff354851427f4c4fab4f9c8619ab789c
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-04-15 18:25:57 -0300

    Finish Book class

--- src/main/java/br/com/syscrud/model/Book.java
+++ src/main/java/br/com/syscrud/model/Book.java
@@ -1,5 +1,6 @@
 package br.com.syscrud.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
@@ -7,7 +8,34 @@ import jakarta.persistence.Table;
 @Table(name = "book")
 public class Book {
 	
+	@Column(name = "author")
 	private String author;
 	
+	@Column(name = "genre")
 	private String genre;
+	
+	public Book(String author, String genre) {
+		this.author = author;
+		this.genre = genre;
+	}
+
+	public Book() {}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
 }
+
